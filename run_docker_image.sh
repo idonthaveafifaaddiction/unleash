6c11a513c2f3c5d2baff3885d3ac6aa621ab0753
Nihar Dongara <nihar.dongara@copart.com>
2020-08-06 16:20:49 -0500

    build: Dockerized the application and introduced cloud config.

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -4,5 +4,4 @@ profiles=${PROFILES:-test}
 app_name=${SPRING_APPLICATION_NAME:-unleash}
 config_user=${CONFIGSERVER_USER:-test}
 password=${CONFIGSERVER_PASSWORD:-test}
-access_log_loc=/opt/copart/logs
 NODE_ENV=production CONFIGSERVER_NAME=$endpoint_url APP=$app_name PROFILES=$profiles CONFIGSERVER_PASSWORD=$password CLOUD_CONFIG_USER=$config_user npm run start
